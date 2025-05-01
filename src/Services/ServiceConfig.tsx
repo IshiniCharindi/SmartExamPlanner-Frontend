0c0b1f08d69ceddcba61cfab8928aa3a2ee8bb1f
Ishini Dewamiththa <ishinidewamiththa@gmail.com>
2025-05-01 13:15:17 +0530

    configure

---
+++ src/Services/ServiceConfig.tsx
@@ -0,0 +1,52 @@
+import axios from 'axios'
+
+
+const BEARER_TOKEN = 'Bearer dQc0W8Sb384CyCcIsQ8UMTrKlCOTf2wHGElAxlkHArfvzjaPsZ'
+const PUBLIC_ADDRESS = 'http://localhost:8001/'
+const localDomain = axios.create({
+    baseURL: 'http://localhost:3001',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    // timeout: 10000,
+})
+
+
+const headers = {
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': BEARER_TOKEN
+    }
+}
+
+
+const fileUploadHeaders = {
+    headers: {
+        'Accept': 'application/json',
+    }
+}
+
+
+
+const domain = localDomain
+
+
+domain.interceptors.response.use(
+    response => {
+        return response
+    },
+
+    error => {
+        console.error(error)
+        return error.response
+    }
+)
+
+
+export default domain
+export {
+    headers,
+    fileUploadHeaders,
+    PUBLIC_ADDRESS
+}
