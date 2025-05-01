77f72297c62a4fa2c1f30e32c47d988d6343f854
Ishini Dewamiththa <ishinidewamiththa@gmail.com>
2025-05-01 12:03:33 +0530

    created folder strucutre

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import ScrollToTop from "./Components/Other/ScrollToTop.tsx";
+import BaseHome from "./Features/BaseHome.tsx";
 function App() {
 
 
   return (
     <>
-
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route element={<BaseHome/>} path='/'/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/Features/BaseHome.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BaseHome = () => {
+    return (
+        <div className="BaseHome">
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+export default BaseHome;
