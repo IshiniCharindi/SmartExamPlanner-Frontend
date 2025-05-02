0f834e0f1bb4787fdd844110b484cabc151673ee
K.L.T. Lakchani <169138117+tharusha-liyanage@users.noreply.github.com>
2025-05-02 09:14:16 +0530

    Update BaseHome.tsx

--- src/Features/BaseHome.tsx
+++ src/Features/BaseHome.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import StudentDataForm from "../Components/ExamShedule/StudentDataForm";
+import SignUp from "../Components/signup/signup";
 
 
 const BaseHome = () => {
     return (
         <div className="BaseHome">
         <StudentDataForm/>
+        <SignUp/>
         </div>
     );
 };
